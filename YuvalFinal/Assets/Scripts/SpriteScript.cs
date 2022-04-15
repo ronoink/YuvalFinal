d11aedcbc37667b60fc18a471f64d731f7f28833
ronoink <ronoink@gmail.com>
2022-04-15 17:49:40 +0300

    Click To Move Controller Test 2
    Click To Move Controller Test 2

--- YuvalFinal/Assets/Scripts/SpriteScript.cs
+++ YuvalFinal/Assets/Scripts/SpriteScript.cs
@@ -1,23 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class SpriteScript : MonoBehaviour
 {
 
     public Transform target;
     public float zOffset;
+
+    private Animator animator;
+    public NavMeshAgent myAgent;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        animator = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (myAgent.velocity.x == 0)
+        {
+            animator.SetInteger("Direction", 0);
+        }
+        else if (myAgent.velocity.z > .5f)
+        {
+            animator.SetInteger("Direction", 1);
+        }  
+         else if (myAgent.velocity.z < -.5f)
+        {
+            animator.SetInteger("Direction", 2);
+        }  
+        else if (myAgent.velocity.x > .5f)
+        {
+            animator.SetInteger("Direction", 3);
+        }  
+        else if (myAgent.velocity.x < -.5f)
+        {
+            animator.SetInteger("Direction", 4);
+        }
     }
 
     void LateUpdate()
