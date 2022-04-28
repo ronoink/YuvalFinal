0543b839e243b711bc24d33b6a10b39f8c7607df
ronoink <ronoink@gmail.com>
2022-04-28 16:52:10 +0300

    Scene 3 With Timer
    Scene 3 With Timer

---
+++ YuvalFinal/Assets/Scripts/Scene2/NextScene.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour
+{
+    public GameObject timer;
+    public float timeRemains;
+    
+    // Start is called before the first frame update
+    void Start()
+    { 
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeRemains = timer.GetComponent<Timer2>().timeRemaining;   
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            
+            PlayerPrefs.SetFloat("SetTimer2" , timeRemains);
+            print("nextScene");
+        SceneManager.LoadScene(2);
+
+
+
+
+    }
+    
+
+    
+}
