b07d47503fb7619b16c12ef75e0bf86e6116cba5
ronoink <ronoink@gmail.com>
2022-06-01 13:01:11 +0300

    yard Scene Fixed
    yard Scene Fixed

---
+++ YuvalFinal/Assets/Scene5Button.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene5Button : MonoBehaviour
+{
+    public void button()
+    {
+        print("Button5Ok");
+    }
+}
