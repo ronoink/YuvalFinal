d644811004df27c76bbc4f03e51f8bbae6b14918
ronoink <ronoink@gmail.com>
2022-06-14 01:51:27 +0300

    LastScene1
    LastScene1

---
+++ YuvalFinal/Assets/NextsceneTransition13.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class NextsceneTransition13 : MonoBehaviour
+{
+    public void nextscene()
+    { 
+        SceneManager.LoadScene(20);
+    }
+}
